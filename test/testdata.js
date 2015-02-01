74e77ce5de169764855cc9689ccd7f25c4c87593
Frank Grimm <git@frankgrimm.net>
2015-02-01 22:13:20 +0100

    Add negation handling and emoticons

--- test/testdata.js
+++ test/testdata.js
@@ -2,14 +2,14 @@
 
 var fs = require('fs');
 
-var testdata = fs.readFileSync('./data/test.txt', {encoding: 'utf-8'}).split("\n\n").filter(function(p) { return p && p.length > 1;});
+var testdata = fs.readFileSync('./data/test.txt', {encoding: 'utf-8'}).split("\n\n").map(function(p) { return p.replace(/^\s+|\s+$/g, ''); }).filter(function(p) { return p && p.length > 1;});
 
 var evaluate = function evaluate(score) {
     for (var idx = 0; idx < testdata.length; idx++) {
         var text = testdata[idx];
 
-        score(text, function(err, input, score, assessments) {
-            console.log('"' + text + '": ' + score, '(' + assessments.length + " assessments)");
+        score(text, function(err, input, score, confidence, assessments) {
+            console.log('"' + text + '": ' + score, '[confidence: ' + confidence + '%]', '(' + assessments.length + " token infos)");
         });
     }
 };
