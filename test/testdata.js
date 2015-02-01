330bf8b6255bec1397db67cf0a6a16509c415341
Frank Grimm <git@frankgrimm.net>
2015-02-01 18:45:19 +0100

    Add simple tokenizer test runner

--- test/testdata.js
+++ test/testdata.js
@@ -14,4 +14,22 @@ var evaluate = function evaluate(score) {
     }
 };
 
-module.exports = exports = {evaluate: evaluate};
+var tokenize = function tokenize(Tokenizer) {
+    for (var idx = 0; idx < testdata.length; idx++) {
+        var text = testdata[idx];
+
+        Tokenizer(text, function(err, tokenized) {
+            if (err) {
+                throw err;
+            }
+            if (!tokenized) {
+                throw new Error('no result for input ' + text);
+            }
+            tokenized = tokenized.map(function (token) { return '"' + token + '"'});
+
+            console.log('"' + text + '" => ' + tokenized.join(', '));
+        });
+    }
+}
+
+module.exports = exports = {evaluate: evaluate, tokenize: tokenize};
---
+++ test/tokenizer.js
@@ -0,0 +1,7 @@
+'use strict';
+
+var Tokenizer = require('../lib/naive_tokenizer.js');
+
+var testdata = require('./testdata.js');
+testdata.tokenize(Tokenizer);
+
