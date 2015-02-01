bedb3981202dac7ea0306d90c9cd9fc51e7335d2
Frank Grimm <git@frankgrimm.net>
2015-02-01 13:57:45 +0100

    SentiWS parser update

--- test/sentiws.js
+++ test/sentiws.js
@@ -3,8 +3,12 @@
 var sentiments = require('../lib/sentiments');
 var testdata = require('./testdata');
 
-sentiments({wordlist: 'sentiws'}, function(score) {
-
+sentiments({wordlist: 'sentiws'}, function(err, score) {
+    if (err) {
+        console.log('Failed to load:');
+        console.log(err);
+        return;
+    }
     
 
 });
