6e6f6c358d278db408536fd7f390d520c89057bf
Frank Grimm <git@frankgrimm.net>
2015-02-01 13:56:43 +0100

    BAWL-R parsing

--- test/bawlr.js
+++ test/bawlr.js
@@ -3,6 +3,11 @@
 var sentiments = require('../lib/sentiments');
 var testdata = require('./testdata');
 
-sentiments({wordlist: 'bawlr', datadirectory: '../data/'}, function(score) {
-
+sentiments({wordlist: 'bawlr', datadirectory: '../data/'}, function(err, score) {
+    if (err) {
+        console.log('Failed to load:');
+        console.log(err);
+        return;
+    }
+    console.log('loaded');
 });
