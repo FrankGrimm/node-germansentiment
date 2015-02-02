9fcb1135b56fddc460022295bb7df4b553e338d1
Frank Grimm <git@frankgrimm.net>
2015-02-02 11:34:49 +0100

    Add __dirname for default file locations

--- test/bawlr.js
+++ test/bawlr.js
@@ -5,7 +5,7 @@
 var sentiments = require('../lib/sentiments');
 var testdata = require('./testdata');
 
-sentiments({wordlist: 'bawlr', datadirectory: '../data/'}, function(err, score) {
+sentiments({wordlist: 'bawlr', datadirectory: __dirname + '/../../data/'}, function(err, score) {
     if (err) {
         console.log('Failed to load:');
         console.log(err);
--- test/testdata.js
+++ test/testdata.js
@@ -2,7 +2,7 @@
 
 var fs = require('fs');
 
-var testdata = fs.readFileSync('./data/test.txt', {encoding: 'utf-8'}).split("\n\n").map(function(p) { return p.replace(/^\s+|\s+$/g, ''); }).filter(function(p) { return p && p.length > 1;});
+var testdata = fs.readFileSync(__dirname + '/../data/test.txt', {encoding: 'utf-8'}).split("\n\n").map(function(p) { return p.replace(/^\s+|\s+$/g, ''); }).filter(function(p) { return p && p.length > 1;});
 
 var evaluate = function evaluate(score) {
     for (var idx = 0; idx < testdata.length; idx++) {
